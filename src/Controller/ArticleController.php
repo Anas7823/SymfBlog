61eacb0754fbc1160e2a10958b7ece45ccb175ab
Anas7823 <anaselkhiat78@gmail.com>
2023-11-24 15:23:22 +0100

    projet fini

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,151 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Article;
+use App\Entity\Categorie;
+use App\Repository\CategorieRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use App\Form\ArticleType;
+use App\Repository\ArticleRepository;
+use App\Form\CommentaireType;
+use App\Entity\Commentaire;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+class ArticleController extends AbstractController
+{
+    public function __construct(private EntityManagerInterface $em, private ArticleRepository $ArticleRepository, private CategorieRepository $categorieRepository, private Security $security)
+    {
+    }
+
+    #[Route('/article', name: 'app_article')]
+    public function index(Request $request): Response
+    {
+        $article = new Article();
+        $form = $this->createForm(ArticleType::class, $article);
+        $form->handleRequest($request);
+        $auteur = $this->security->getUser();
+        $article->setAuteur($auteur);
+
+        if ($form->isSubmitted() && !$form->isValid()) {
+            $this->addFlash('danger', "Le formulaire contient des erreurs");
+        } elseif ($form->isSubmitted() && $form->isValid()) {
+            // Vérifier si l'état est égal à 1 (Publié)
+            if ($article->isEtat() == 1) {
+                // Si l'état est égal à 1, générer la date de publication
+                $article->setDateParu(new \DateTimeImmutable());
+            } else {
+                // Si l'état n'est pas égal à 1, définir dateParu sur null
+                $article->setDateParu(null);
+            }
+            if (!$auteur){
+                $this->addFlash('danger', "Vous devez être connecté pour ajouter un article");
+                return $this->redirectToRoute('app_article');
+            } else{
+                $this->em->persist($article);
+                $this->em->flush();
+                $this->addFlash('success', "L'article a bien été ajouté");
+                return $this->redirectToRoute('app_article');
+
+            }
+        }
+
+        return $this->render('article/index.html.twig', [
+            'controller_name' => 'ArticleController',
+            'form' => $form->createView(),
+            'articles' => $this->ArticleRepository->findAll()
+        ]);
+    }
+
+    #[Route('/article-details/{id}', name: 'detail_article')]
+    public function details(int $id, Article $article = null, Request $request, CommentaireController $commentaireController)
+    {
+        if ($article == null) {
+            return $this->redirectToRoute('app_article');
+        }
+        $article = $this->ArticleRepository->find($id); 
+        $categorie = $article->getCategorie();
+        $auteur = $article->getAuteur();
+        $auteurComm = $this->getUser(); 
+        $commentaire = new Commentaire();
+        $commentaire->setAuteur($auteurComm);
+        $commentaire->setArticle($article);
+        $commentaires = $article->getCommentaires();
+        
+        $commentaireForm = $commentaireController->createForm(CommentaireType::class, $commentaire);
+        $commentaireForm->handleRequest($request);
+    
+        if ($commentaireForm->isSubmitted() && $commentaireForm->isValid()) {
+            $this->em->persist($commentaire);
+            $this->em->flush();
+            $this->addFlash('success', 'Le commentaire a bien été ajouté.');
+            return $this->redirectToRoute('detail_article', ['id' => $id]);
+        } elseif ($commentaireForm->isSubmitted()) {
+            $this->addFlash('danger', 'Le commentaire n\'a pas été ajouté.');
+        }
+    
+        return $this->render('article/details.html.twig', [
+            'articles' => $article,
+            'commentaireForm' => $commentaireForm->createView(),
+            'commentaires' => $commentaires,
+            'categorie' => $categorie
+        ]);
+    }
+
+    #[IsGranted('ROLE_ADMIN')]
+    #[Route('/article-modifier/{id}', name: 'modifier_article')]
+    public function modifier(Request $request, Article $article = null)
+    {
+        if ($article == null) {
+            return $this->redirectToRoute('app_article');
+        } else {
+            $form = $this->createForm(ArticleType::class, $article);
+            $form->handleRequest($request);
+
+            if ($form->isSubmitted() && !$form->isValid()) {
+                $this->addFlash('danger', "Le formulaire contient des erreurs");
+            } elseif ($form->isSubmitted() && $form->isValid()) {
+                // Vérifier si l'état est égal à 1 (Publié)
+                if ($article->isEtat() == 1) {
+                    // Si l'état est égal à 1, générer la date de publication
+                    $article->setDateParu(new \DateTimeImmutable());
+                } else {
+                    // Si l'état n'est pas égal à 1, définir dateParu sur null
+                    $article->setDateParu(null);
+                }
+
+                $this->em->persist($article);
+                $this->em->flush();
+                $this->addFlash('success', "L'article a bien été modifié");
+                return $this->redirectToRoute('app_article');
+            }
+
+            return $this->render('article/modifier.html.twig', [
+                'form' => $form->createView(),
+                'article' => $article
+            ]);
+        }
+    }
+
+    #[IsGranted('ROLE_ADMIN')]
+    #[Route('/article-supprimer/{id}', name: 'supprimer_article')]
+    public function supprimer(Article $article = null)
+    {
+        if ($article == null) {
+            return $this->redirectToRoute('app_article');
+        } else {
+            foreach ($article->getCommentaires() as $commentaire){
+                $article->removeCommentaire($commentaire);
+            }
+            $this->em->remove($article);
+            $this->em->flush();
+            $this->addFlash('success', "L'article a bien été supprimé");
+            return $this->redirectToRoute('app_article');
+        }
+    }
+}
---
+++ src/Entity/Article.php
@@ -0,0 +1,170 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ArticleRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ArticleRepository::class)]
+class Article
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $titre = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $contenu = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $dateCrea = null;
+
+    #[ORM\Column]
+    private ?bool $etat = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?\DateTimeImmutable $dateParu = null;
+
+    #[ORM\ManyToOne(inversedBy: 'articles')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?User $auteur = null;
+
+    #[ORM\ManyToOne(inversedBy: 'articles')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Categorie $categorie = null;
+
+    #[ORM\OneToMany(mappedBy: 'Article', targetEntity: Commentaire::class)]
+    private Collection $commentaires;
+
+    public function __construct()
+    {
+        $this->commentaires = new ArrayCollection();
+        $this->dateCrea = new \DateTimeImmutable();
+        $this->dateParu = new \DateTimeImmutable();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): static
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    public function getContenu(): ?string
+    {
+        return $this->contenu;
+    }
+
+    public function setContenu(?string $contenu): static
+    {
+        $this->contenu = $contenu;
+
+        return $this;
+    }
+
+    public function getDateCrea(): ?\DateTimeImmutable
+    {
+        return $this->dateCrea;
+    }
+
+    public function setDateCrea(\DateTimeImmutable $dateCrea): static
+    {
+        $this->dateCrea = $dateCrea;
+
+        return $this;
+    }
+
+    public function isEtat(): ?bool
+    {
+        return $this->etat;
+    }
+
+    public function setEtat(bool $etat): static
+    {
+        $this->etat = $etat;
+
+        return $this;
+    }
+
+    public function getDateParu(): ?\DateTimeImmutable
+    {
+        return $this->dateParu;
+    }
+
+    public function setDateParu(?\DateTimeImmutable $dateParu): static
+    {
+        $this->dateParu = $dateParu;
+
+        return $this;
+    }
+
+    public function getAuteur(): ?User
+    {
+        return $this->auteur;
+    }
+
+    public function setAuteur(?User $auteur): static
+    {
+        $this->auteur = $auteur;
+
+        return $this;
+    }
+
+    public function getCategorie(): ?Categorie
+    {
+        return $this->categorie;
+    }
+
+    public function setCategorie(?Categorie $categorie): static
+    {
+        $this->categorie = $categorie;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Commentaire>
+     */
+    public function getCommentaires(): Collection
+    {
+        return $this->commentaires;
+    }
+
+    public function addCommentaire(Commentaire $commentaire): static
+    {
+        if (!$this->commentaires->contains($commentaire)) {
+            $this->commentaires->add($commentaire);
+            $commentaire->setArticle($this);
+        }
+
+        return $this;
+    }
+
+    public function removeCommentaire(Commentaire $commentaire): static
+    {
+        if ($this->commentaires->removeElement($commentaire)) {
+            // set the owning side to null (unless already changed)
+            if ($commentaire->getArticle() === $this) {
+                $commentaire->setArticle(null);
+            }
+        }
+
+        return $this;
+    }
+}
